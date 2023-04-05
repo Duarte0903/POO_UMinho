5de0c4a9e5dd6b768c805292b5a9972b3753a3cd
Duarte Leitão <duarte0903@gmail.com>
2023-04-05 15:14:32 +0100

    initial commit

---
+++ src/entidades/Artigo.java
@@ -0,0 +1,8 @@
+package entidades;
+
+public class Artigo
+{
+    private String codigo;
+
+    private Transportadora transportadora;
+}
---
+++ src/entidades/User.java
@@ -0,0 +1,12 @@
+package entidades;
+
+public class User
+{
+    private String codigo;
+
+    private String nome;
+
+    private int nif;
+
+    private String email;
+}
